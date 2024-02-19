07575c78660ed0a75db1fd5677169965175d5ec1
islom42 <islomnumanov.uz@gmail.com>
2024-02-20 00:45:46 +0300

    feat: Add string template

--- 03_dars/index.js
+++ 03_dars/index.js
@@ -3,5 +3,6 @@ const age = 23;
 const bool = false;
 const boolString = String(bool) // === "false"
 const ageString = String(age); // "23"
-
-console.log(typeof ageString, typeof boolString,ageString, boolString)
+const ageTemplateString = `${age}`
+console.log(typeof ageString, typeof boolString,ageString, boolString);
+console.log(typeof ageTemplateString);
