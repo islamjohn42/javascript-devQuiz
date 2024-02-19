da19303a2a63f1316d07d137c52549676985f68d
islom42 <islomnumanov.uz@gmail.com>
2024-02-19 10:24:27 +0300

    feat: Add example type changing

---
+++ 03_dars/index.js
@@ -0,0 +1,7 @@
+// Type changing
+const age = 23;
+const bool = false;
+const boolString = String(bool) // === "false"
+const ageString = String(age); // "23"
+
+console.log(typeof ageString, typeof boolString,ageString, boolString)
