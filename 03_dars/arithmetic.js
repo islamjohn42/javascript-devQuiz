5679e09a1410ebc9d7675340e0b9fc500469c2bc
islom42 <islomnumanov.uz@gmail.com>
2024-02-21 00:59:01 +0300

    feat: Add comparision and arithmetic operators

---
+++ 03_dars/arithmetic.js
@@ -0,0 +1,15 @@
+//! Arithmetical operators
+/* 
++
+- 
+/ 
+%
+**
+*/
+const num1 = 5;
+const num2 = 2;
+console.log(num1 + num2);
+console.log(num1 / num2);
+console.log(num1 % num2);
+console.log(num1 ** num2);
+console.log((num1 + num2) * 2);
