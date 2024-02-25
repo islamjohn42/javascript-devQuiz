f90d649411257e4a741832c12e945d7f985aba23
islom42 <islomnumanov.uz@gmail.com>
2024-02-26 00:21:12 +0300

    feat: Add expression and declaration types of functions

--- 05_dars/function.js
+++ 05_dars/function.js
@@ -15,3 +15,22 @@ function calcArg(arg1, ...rest) {
   console.log(arg1 + rest.length);
 }
 calcArg(32, 1, 2, 3)
+
+// !===========================
+// Function declaration
+function sayHi(message) {
+  console.log("Hello my function type is " + message);
+}
+sayHi("declaration")
+
+// Function expression
+const sayHello = function(message) {
+  console.log("Hello my function type is " + message);
+}
+sayHello("expression")
+
+function getGreeting(message) {
+  console.log("Hey my function type also is " + message);
+}
+const getGreetingFn = getGreeting
+getGreetingFn("expression")
