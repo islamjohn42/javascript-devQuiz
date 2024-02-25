637d1f22a9b51655240137cbc6566b1a41a3ff4d
islom42 <islomnumanov.uz@gmail.com>
2024-02-25 19:15:58 +0300

    feat: Add loops

---
+++ 04_dars/loops.js
@@ -0,0 +1,19 @@
+// ! loops: for, while, do while
+for(let i = 0; i <= 10; i+=2) {
+  console.log(i);
+}
+
+// ! while
+let j = 0;
+while(j < 4){
+  j++
+  console.log(j);
+}
+// console.log(j); 4
+
+// ! do while
+
+let k = "k";
+do{
+  console.log("Do: ", k);
+}while(k === 5)
