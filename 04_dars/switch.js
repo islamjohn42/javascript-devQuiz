6a8297ff069b1effff9584b0acd22bfdda7e4acb
islom42 <islomnumanov.uz@gmail.com>
2024-02-25 19:43:48 +0300

    feat: Add switch structure and example

---
+++ 04_dars/switch.js
@@ -0,0 +1,40 @@
+// switch case
+let dayOfWeek = 5;
+
+
+// switch(dayOfWeek){
+//   case 1:
+//     console.log("Monday");
+//     break;
+//   case 2:
+//     console.log("Tuesday");
+//     break;
+//   case 3:
+//     console.log("Wednesday");
+//     break;
+//   case 4:
+//     console.log("Thursday");
+//     break;
+//   case 5:
+//     console.log("Friday");
+//     break;
+//   case 6:
+//     console.log("Saturday");
+//     break;
+//   case 1:
+//     console.log("Sunday");
+//     break;
+//   default: "Not found"
+// }
+
+const dictionary = "clever";
+switch (dictionary) {
+  case "smart":
+  case "clever":
+    console.log("Aqilli");
+    break;
+  case "interesting":
+    console.log("Qiziqarli");
+    break;
+  default: "Not found"
+}
