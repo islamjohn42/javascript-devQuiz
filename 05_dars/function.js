8419f73fb00a664356b79a0c6c707b7ebb44f4b9
islom42 <islomnumanov.uz@gmail.com>
2024-02-26 00:12:15 +0300

    feat: Add calc and rest operator to function

--- 05_dars/function.js
+++ 05_dars/function.js
@@ -3,4 +3,15 @@
 function getName(arg1, arg, ...n) {
   //code block
 }
-getName()
+getName();
+
+//! Function with parametr
+function calc(a, b) {
+  console.log(a + b);
+}
+calc(2, 3);
+
+function calcArg(arg1, ...rest) {
+  console.log(arg1 + rest.length);
+}
+calcArg(32, 1, 2, 3)
